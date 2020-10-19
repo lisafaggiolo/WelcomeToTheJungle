d2f3e683a726d85b97190ae292051055a9c5a553
Lisa <65428933+lisafaggiolo@users.noreply.github.com>
2020-10-19 13:41:09 -0500

    Feature/user-auth (#7)
    * new About Us page added.
    
    * Admin Category page added - admin can now create or delete categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,12 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  resources :about, only: [:index]
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
+    resources :categories, except: [:edit, :update, :show]
     resources :products, except: [:edit, :update, :show]
   end
 
