e13884583dbcd48cd7282413fec4df6291c464d9
Lisa <65428933+lisafaggiolo@users.noreply.github.com>
2020-10-21 21:49:59 -0500

    Feature/product specs (#10)
    * added testing for product spec.
    
    * added password verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
+     puts session[1]
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  before(:each) do
+    @product = Product.new(
+      :name => "nice_glasses",
+      :description => "to look better",
+      :category_id => 1,
+      :quantity => 10,
+      :image => "this is an image",
+      :price => 100)
+  end
+  
+  describe "Validations" do
+    it "has .name" do
+      @product.name.presence == true   
+    end
+
+    it "has .price" do
+      @product.price.presence == true   
+    end
+
+    it "has .quantity" do
+      @product.quantity.presence == true   
+    end
+
+    it "has .category" do
+      @product.category.presence == true   
+    end
+  end
+end
